dc94e2310c7255775f55ce91162d73f540a3e4a8
Abhishek Patel <ap975498@gmail.com>
2025-04-10 16:52:28 +0530

    update

--- env_example.js
+++
@@ -1,8 +0,0 @@
-MONGO_URI="mongodb://localhost:27017/exam_portal"
-PORT=8000
-JWT_SECRET="secret"
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT=587
-EMAIL_USER="noobabhi015@gmail.com"
-EMAIL_PASS="#Khyal1974"
-EMAIL_FROM="ap975498@gmail.com"
